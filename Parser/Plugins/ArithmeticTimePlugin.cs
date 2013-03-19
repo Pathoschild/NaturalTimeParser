1f84f8972544556a073cc8797ecbac9222666938
Jesse Plamondon-Willard <jplamondonw@gmail.com>
2013-03-19 13:02:27 -0400

    + remaining relative time support, localization

--- Parser/Plugins/ArithmeticTimePlugin.cs
+++ Parser/Plugins/ArithmeticTimePlugin.cs
@@ -6,16 +6,67 @@ using Pathoschild.NaturalTimeParser.Parser.Tokenization;
 namespace Pathoschild.NaturalTimeParser.Parser.Plugins
 {
 	/// <summary>Parses a natural time format string containing time arithmetic (like "+2 days") into a set of tokens.</summary>
+	/// <remarks>This is an implementation of GNU relative items: http://www.gnu.org/software/tar/manual/html_node/Relative-items-in-date-strings.html </remarks>
 	public class ArithmeticTimePlugin : IParseTimeStrings, IApplyTimeTokens
 	{
 		/*********
-		** Accessors
+		** Properties
 		*********/
 		/// <summary>The regular expression that matches the date tokens in the input expression.</summary>
-		protected Regex ParsePattern = new Regex(@"^(?<expression>\s*(?<sign>[\+\-]{0,1})\s*(?<value>\d+)\s*\b(?<unit>[a-zA-Z]+)\b)+$", RegexOptions.Compiled);
+		protected readonly Regex ParsePattern = new Regex(@"^(?<expression>\s*((?<sign>[\+\-]{0,1})\s*(?<value>\d*))?\s*\b(?<unit>[a-zA-Z]+)\b(?<negate>(\s*\bago\b)?)\s*)+", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
 
 		/// <summary>The arbitrary key which identifies this plugin.</summary>
 		protected const string Key = "Arithmetic";
+
+
+		/*********
+		** Accessors
+		*********/
+		/// <summary>A supported relative time unit.</summary>
+		public enum RelativeTimeUnit
+		{
+			/// <summary>A unit of one second.</summary>
+			Seconds,
+
+			/// <summary>A unit of one minute.</summary>
+			Minutes,
+
+			/// <summary>A unit of one hour.</summary>
+			Hours,
+
+			/// <summary>A unit of one day.</summary>
+			Days,
+
+			/// <summary>A unit of seven days.</summary>
+			Weeks,
+
+			/// <summary>A unit of fourteen days.</summary>
+			Fortnights,
+
+			/// <summary>A unit of one month.</summary>
+			Months,
+
+			/// <summary>A unit of one year.</summary>
+			Years,
+
+			/// <summary>An unknown unit of time.</summary>
+			Unknown
+		};
+
+		/// <summary>The supported time units.</summary>
+		/// <remarks>This provides a case-insensitive unit lookup when parsing relative time items. The optional -s suffix is stripped before this lookup.</remarks>
+		public readonly IDictionary<string, RelativeTimeUnit> SupportedUnits = new Dictionary<string, RelativeTimeUnit>(StringComparer.InvariantCultureIgnoreCase)
+		{
+			{ "sec", RelativeTimeUnit.Seconds },
+			{ "second", RelativeTimeUnit.Seconds },
+			{ "min", RelativeTimeUnit.Minutes },
+			{ "minute", RelativeTimeUnit.Minutes },
+			{ "hour", RelativeTimeUnit.Hours },
+			{ "day", RelativeTimeUnit.Days },
+			{ "week", RelativeTimeUnit.Weeks },
+			{ "month", RelativeTimeUnit.Months },
+			{ "year", RelativeTimeUnit.Years }
+		};
 
 
 		/*********
@@ -35,12 +86,27 @@ namespace Pathoschild.NaturalTimeParser.Parser.Plugins
 			int patternCount = match.Groups["expression"].Captures.Count;
 			for (int i = 0; i < patternCount; i++)
 			{
+				// extract parts
 				string expression = match.Groups["expression"].Captures[i].Value;
-				int value = int.Parse(match.Groups["value"].Captures[i].Value);
-				if (match.Groups["sign"].Captures[i].Value == "-")
+				string sign = match.Groups["sign"].Captures[i].Value;
+				string rawValue = match.Groups["value"].Captures[i].Value;
+				string rawUnit = match.Groups["unit"].Captures[i].Value;
+				bool negate = match.Groups["negate"].Captures[i].Value.Length > 0;
+
+				// parse value
+				int value = string.IsNullOrWhiteSpace(rawValue) ? 1 : int.Parse(rawValue);
+				if (sign == "-")
 					value *= -1;
-				string unit = match.Groups["unit"].Captures[i].Value;
-				yield return new TimeToken(ArithmeticTimePlugin.Key, expression, unit, value);
+				if (negate)
+					value *= -1; // note: double-negation (like -1 year ago) is valid
+
+				// parse unit
+				RelativeTimeUnit unit = this.ParseUnit(rawUnit);
+				if (unit == RelativeTimeUnit.Unknown)
+					yield break; // unsupported unit
+
+				// return token
+				yield return new TimeToken(ArithmeticTimePlugin.Key, expression, unit.ToString(), value);
 			}
 		}
 
@@ -53,39 +119,52 @@ namespace Pathoschild.NaturalTimeParser.Parser.Plugins
 			// parse token
 			if (token.Parser != ArithmeticTimePlugin.Key || !(token.Context is int))
 				return null;
-			string unit = token.Value.ToLower();
+			RelativeTimeUnit unit = (RelativeTimeUnit)Enum.Parse(typeof(RelativeTimeUnit), token.Value);
 			int value = (int)token.Context;
 
 			// apply
 			switch (unit)
 			{
-				case "second":
-				case "seconds":
+				case RelativeTimeUnit.Seconds:
 					return date.AddSeconds(value);
 
-				case "minute":
-				case "minutes":
+				case RelativeTimeUnit.Minutes:
 					return date.AddMinutes(value);
 
-				case "hour":
-				case "hours":
+				case RelativeTimeUnit.Hours:
 					return date.AddHours(value);
 
-				case "day":
-				case "days":
+				case RelativeTimeUnit.Days:
 					return date.AddDays(value);
 
-				case "month":
-				case "months":
+				case RelativeTimeUnit.Weeks:
+					return date.AddDays(value * 7);
+
+				case RelativeTimeUnit.Fortnights:
+					return date.AddDays(value * 14);
+
+				case RelativeTimeUnit.Months:
 					return date.AddMonths(value);
 
-				case "year":
-				case "years":
+				case RelativeTimeUnit.Years:
 					return date.AddYears(value);
 
 				default:
 					throw new FormatException(String.Format("Invalid arithmetic time unit: {0}", unit));
 			}
+		}
+
+
+		/*********
+		** Protected methods
+		*********/
+		/// <summary>Parse a localized time unit (like "secs") into a <see cref="RelativeTimeUnit"/>.</summary>
+		/// <param name="unit">The localized time unit.</param>
+		protected RelativeTimeUnit ParseUnit(string unit)
+		{
+			return this.SupportedUnits.ContainsKey(unit)
+				? this.SupportedUnits[unit]
+				: RelativeTimeUnit.Unknown;
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-**Pathoschild.NaturalTimeParser** provides a partial C# implementation of natural time formats in the
+﻿**Pathoschild.NaturalTimeParser** provides a partial C# implementation of natural time formats in the
 [GNU date input format](http://www.gnu.org/software/tar/manual/html_node/Date-input-formats.html).
 This currently lets you perform arithmetic with natural date strings (like
 `DateTime.Now.Offset("+5 days 14 hours -2 minutes")`), and will eventually support creating
@@ -11,19 +11,48 @@ welcome.
 Download the `Pathoschild.NaturalTimeParser` NuGet package (TODO) and reference the
 `Pathoschild.NaturalTimeParser` namespace. This lets you apply a natural offset to a date:
 ```c#
-   DateTime.Now.Offset("-15 minutes 4 hours 16 years");
-   TimeParser.Default.Parse("-2 hours");
+   // both lines are equivalent
+   DateTime.Now.Offset("2 years ago");
+   TimeParser.Default.Parse("2 years ago");
 ```
 
-## Implementation
+### Relative time units (date arithmetic)
+The parser has full support for [relative time units](http://www.gnu.org/software/tar/manual/html_node/Relative-items-in-date-strings.html#SEC125). For example, the following formats are supported:
+
+* `1 year ago`
+* `-2 years`
+* `16 fortnights`
+* `-1 year ago` (next year)
+  
+You can also chain relative units:
+
+* `1 year 2 months` (14 months from now)
+* `1 year -2 fortnights` (almost 11 months from now)
+* `1 year ago 1 year` (today; equivalent to `-1 year +1 year`)
+
+## Extending support
+### Localization
+The default implementation is English but can support other languages. For example, you can enable
+relative time units in French:
+```c#
+   // configure French units
+   ArithmeticTimePlugin plugin = TimeParser.Default.Parsers.OfType<ArithmeticTimePlugin>().First();
+   plugin.SupportedUnits["jour"] = ArithmeticTimePlugin.RelativeTimeUnit.Days;
+   plugin.SupportedUnits["heure"] = ArithmeticTimePlugin.RelativeTimeUnit.Hours;
+
+   // now you can use French
+   DateTime.Now.Offset("3 jours 4 heures");
+```
+
+### Plugins
 This is implemented as a simple plugin-based lexer, which breaks down an input string into
 its constituent tokens. For example, the string "`yesterday +1 day`" can be broken down into two
 tokens:
 ```js
-    [
-       {"unit": "yesterday"},
-       {"unit": "day", "value": 1}
-    ]
+   [
+      ["yesterday"],
+      ["days", 1]
+   ]
 ```
 
 The parsing is provided by a set of plugins which implement `IParseTimeStrings` or
